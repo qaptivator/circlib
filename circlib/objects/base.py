50f2da32ca2635ac6c84d211a83d3b5fa8dec052
qaptivator <ac@i-ns.net>
2024-09-26 22:32:01 +0300

    update base.py

--- circlib/objects/base.py
+++ circlib/objects/base.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from ..level import Level
+from abc import ABC, abstractmethod
 
-class ObjectType(Enum):
+class ObjectCategory(Enum):
     unknown = 0
     fixed = 1 # fixed circle, triangle, line
     movable = 2 # movable circle, movable triangle
@@ -11,33 +13,40 @@ class ObjectType(Enum):
     generator = 7 # ball geneator, box generator
     special = 8 # portal, start, collectable
 
-class BaseObject:
+class BaseObject(ABC):
+    ''' Base abstract class for all objects. 
+    type: str - 
+    ingame_type: str - used to detect the matching object from level code (e.g. 'fixed circle' object would have this set to 'c')'''
+
     id: int = 0,
-    type: ObjectType = ObjectType.unknown,
+    type: str = '',
     ingame_type: str = '',
 
-    def __init__(self, id=0, type=''):
-        self.id = id
-        self.type = type
+    category: ObjectCategory = ObjectCategory.unknown,
 
-    @staticmethod
-    def from_object(obj):
-        return BaseObject(
-            obj.get("id"),
-            obj.get("type"),
-        )
+    def __init__(self, level: Level, *args):
+        self.level = level
+        self.create(args)
 
-    def is_connection(self):
-        return self.connections and len(self.connections) > 0
-    
-    def get_ingame_type(self):
-        return OBJECT_TYPES.get_inv(self.type)
+    def whoami(self):
+        ''' Returns class name '''
+        return type(self).__name__
 
-    def get_ingame_props(self):
-        return list_to_string(self.props)
-        
-    def get_connections(self):
-        if self.is_connection():
-            return self.connections
-        else:
-            raise TypeError("This object is not a connection")
+    @abstractmethod
+    def create(self, *args):
+        ''' Called on object initialization. '''
+        pass
+
+    @abstractmethod
+    def deserialize(self, object_index: int):
+        ''' Extract object's properties from level code.
+        This would be called on each object during level.parse()
+        object_index specifies the character position of the object's type in level code. '''
+        pass
+
+    @abstractmethod
+    def serialize(self) -> str:
+        ''' Export the object into level code.
+        This would be called on each object during level.stringify() 
+        You need to return valid level code, which will then get appended to the rest of the level. '''
+        pass
